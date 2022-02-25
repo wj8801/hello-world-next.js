c4cff504fefc57b8c4fb55454f44e5d03251cd35
Welington thon <thonciencias@gmail.com>
2022-02-25 15:20:36 -0300

    estudos de apis next.js

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,12 +2,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  name: string,
+  idade: number
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log(req.method),
+  res.status(200).json({ name: 'Wellington', idade: 39 })
 }
